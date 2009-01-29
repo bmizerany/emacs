0a4b5022c98e064afe5436fe611d645af3396159
bmizerany <blake.mizerany@gmail.com>
2009-01-28 19:16:16 -0800

    updated erlang tools dir

--- defunkt/erlang.el
+++ defunkt/erlang.el
@@ -1,4 +1,4 @@
-(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.1/emacs"
+(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.2/emacs"
       load-path))
       (setq erlang-root-dir "/usr/local/lib/erlang")
       (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
